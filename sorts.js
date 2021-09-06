3fc126d349a4d7be7650ae46a87c4c54d27f0385
leoruhl94 <leoruhl94@gmail.com>
2021-09-06 14:14:27 -0300

    Selection sort and Insertion sort added

--- sorts.js
+++ sorts.js
@@ -1,6 +1,6 @@
 
 //________________ Bubble Sort v1.0__________
-function bubbleSortV1(array) {
+const bubbleSortV1 = (array) => {
     let length = array.length, aux;
     for (let i = 0; i < length; i++) {
         for(var j = 0 ; j < length; j++){
@@ -15,7 +15,7 @@ function bubbleSortV1(array) {
   }
 
   //________________ Bubble Sort v2.0__ descriptive________
-function bubbleSortV2(array) {
+const bubbleSortV2 = (array) => {
     let length = array.length, left, right;
     for (let i = 0; i < length; i++) {
         for(var j = 0 ; j < length; j++){
@@ -30,6 +30,43 @@ function bubbleSortV2(array) {
     return array;
   }
 
+//_______________Selection sort V1.0________________
+
+const selectionSortV1= (array) => {
+    let length = array.length, minor, aux;
+    for (let i = 0; i < length; i++) {
+        minor = i;
+        for (let j = i + 1; j < length; j++) {
+            if (array[minor] > array[j]) {
+                minor = j;
+            }
+        }
+        if (minor !== i) {
+            aux = array[i];
+            array[i] = array[minor];
+            array[minor] = aux;
+        }
+    }
+    return array;
+}
+
+//_______________Insertion sort V1.0________________
+
+const insertionSortV1 = (array) => {
+    let length = array.length, current, previous;
+        for (let i = 1; i < length; i++) {
+            current = array[i];
+            previous = i-1; 
+            while ((previous > -1) && (array[previous] > current)) {
+                array[previous + 1] = array[previous];
+                previous--;
+            }
+            array[previous + 1] = current;
+        }
+    return array;
+}
+
+
 // _____ para hacer debug y ver como funciona agregar las siguientes lineas dentro del segundo for_____
 
 //   console.log(`i: ${i} - j: ${j}`);
@@ -41,8 +78,10 @@ function bubbleSortV2(array) {
   let arrayUnordered = [5, 2, 8, 9, 0, 3, 1, 4, 6, 7];
   let arrayBubbleV1 = bubbleSortV1(arrayUnordered);
   let arrayBubbleV2 = bubbleSortV2(arrayUnordered);
+  let arraySelectionV1 = selectionSortV1(arrayUnordered);
+  let arrayInsertionV1 = insertionSortV1(arrayUnordered);
   console.log('Array Unordered= ', arrayUnordered);
   console.log('Array Ordered BubbleV1= ', arrayBubbleV1);
   console.log('Array Ordered BubbleV2= ', arrayBubbleV2);
-
-
+  console.log('Array Ordered SelectionV1= ', arraySelectionV1);
+  console.log('Array Ordered InsertionV1= ', arrayInsertionV1);
